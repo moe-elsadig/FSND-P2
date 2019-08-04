a8db8e64a22c1bdb5b99f8b780a0408a5aeca1c6
moe-elsadig <m.da7th@gmail.com>
2019-08-04 14:54:46 +0000

    modified the IP and port in the main file

--- __init__.py
+++ __init__.py
@@ -644,5 +644,5 @@ def deleteUser(email):
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    # app.run(host='0.0.0.0', port=8000)
-    app.run(host='167.71.74.107')
+    app.run(host='0.0.0.0', port=80)
+    # app.run(host='167.71.74.107')
